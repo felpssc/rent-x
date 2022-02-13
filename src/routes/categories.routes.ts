bdaac37de85a7b7be2cbdb11ad5400c0e69390a7
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-02-12 21:14:23 -0300

    chore: create Categories CRUD architecture

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
+
+const routes = Router();
+
+const categoriesRepository = new CategoriesRepository();
+
+routes.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ name, description });
+
+  return res.status(201).send();
+});
+
+routes.get("/", (req, res) => {
+  const categories = categoriesRepository.list();
+
+  return res.status(200).json(categories);
+});
+
+export { routes as categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(8000, () => {
   console.log("🔥 Server started running on: http://localhost:8000");
