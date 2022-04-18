43c112ca0e2beaa26355ac12365d75fa23113276
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-04-18 20:39:53 -0300

    refactor: add paths prefix

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -2,7 +2,7 @@ import { Parser } from "csv-parse";
 import fs from "fs";
 import { inject, injectable } from "tsyringe";
 
-import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 
 interface IIMportCategory {
   name: string;
