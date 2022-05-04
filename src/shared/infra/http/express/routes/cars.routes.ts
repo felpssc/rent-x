9c1b4b88268dd2454c4826b07a3c4c227a3296b3
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-05-04 20:42:01 -0300

    fix: list available cars controller

--- src/shared/infra/http/express/routes/cars.routes.ts
+++ src/shared/infra/http/express/routes/cars.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
-import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -9,7 +9,7 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const routes = Router();
 
 const createCarController = new CreateCarController();
-const listAvailableCars = new ListCategoriesController();
+const listAvailableCars = new ListAvailableCarsController();
 
 routes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle);
 routes.get("/available", listAvailableCars.handle);
