f39ef602c244653342f70d5cbdb565a53013263e
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-05-24 18:53:14 -0300

    chore(Category): add e2e tests

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,109 @@
+import { hash } from "bcrypt";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { app } from "@shared/infra/http/express/app";
+
+import createConnection from "../../../../shared/infra/typeorm";
+
+let connection: Connection;
+
+describe("CreateCategoryController", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const passwordHash = await hash("admin", 8);
+
+    const user = await connection.getRepository("users").create({
+      id,
+      name: "Admin",
+      email: "admin@email.com",
+      driver_license: "ABC123XYZ",
+      password: passwordHash,
+      isAdmin: true,
+      created_at: new Date(),
+    });
+
+    await connection.getRepository("users").save(user);
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("Should be able to create a new category", async () => {
+    const responseToken = await request(app).post("/api/sessions").send({
+      email: "admin@email.com",
+      password: "admin",
+    });
+
+    const response = await request(app)
+      .post("/api/categories")
+      .send({
+        name: "teste",
+        description: "teste",
+      })
+      .set("Authorization", `Bearer ${responseToken.body.token}`);
+
+    expect(response.status).toBe(201);
+  });
+
+  it("Should not be able to create a category with existent name", async () => {
+    const responseToken = await request(app).post("/api/sessions").send({
+      email: "admin@email.com",
+      password: "admin",
+    });
+
+    await request(app).post("/api/categories").send({
+      name: "teste",
+      description: "teste",
+    });
+
+    const response = await request(app)
+      .post("/api/categories")
+      .send({
+        name: "teste",
+        description: "teste",
+      })
+      .set("Authorization", `Bearer ${responseToken.body.token}`);
+
+    expect(response.status).toBe(400);
+  });
+
+  it("Should be able to list categories", async () => {
+    const responseToken = await request(app).post("/api/sessions").send({
+      email: "admin@email.com",
+      password: "admin",
+    });
+
+    await request(app)
+      .post("/api/categories")
+      .send({
+        name: "teste",
+        description: "teste",
+      })
+      .set("Authorization", `Bearer ${responseToken.body.token}`);
+
+    await request(app)
+      .post("/api/categories")
+      .send({
+        name: "teste2",
+        description: "teste2",
+      })
+      .set("Authorization", `Bearer ${responseToken.body.token}`);
+
+    const response = await request(app)
+      .get("/api/categories")
+      .set("Authorization", `Bearer ${responseToken.body.token}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(2);
+    expect(response.body[0].name).toBe("teste");
+    expect(response.body[1].name).toBe("teste2");
+  });
+});
--- src/shared/infra/http/express/server.ts
+++ src/shared/infra/http/express/server.ts
@@ -1,39 +1,4 @@
-import "reflect-metadata";
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
-
-import { AppError } from "@shared/errors/AppError";
-import { router } from "@shared/infra/http/express/routes";
-
-import swaggerDocument from "../../../../swagger.json";
-import createConnection from "../../typeorm";
-
-import "@shared/container";
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.use("/api", router);
-
-app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      message: `Internal Server Error - ${err.message}`,
-    });
-  }
-);
+import { app } from "./app";
 
 app.listen(8000, () => {
   console.log(`🔥 Server started running on: 8000`);
